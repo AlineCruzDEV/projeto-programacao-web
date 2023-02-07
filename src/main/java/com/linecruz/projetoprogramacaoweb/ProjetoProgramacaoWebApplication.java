b1b4bc841e5cda80911e5c5b7b62def19fbfb4c7
aline <linegcruz@gmail.com>
2023-02-07 19:35:04 -0300

    Initial commit

---
+++ src/main/java/com/linecruz/projetoprogramacaoweb/ProjetoProgramacaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.linecruz.projetoprogramacaoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoProgramacaoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoProgramacaoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linecruz/projetoprogramacaoweb/ProjetoProgramacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linecruz.projetoprogramacaoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoProgramacaoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
